3ac6bd6f3542c354bd39885e037dc7d397e45853
KadeSan4uk <kotejchik@gmail.com>
2024-12-04 23:12:54 +0200

    BallController

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    
+    private Rigidbody2D _rb;
+    [SerializeField] private float _movementSpeed;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector2 newPosition = _rb.position + Vector2.up * Time.deltaTime * _movementSpeed;
+            _rb.MovePosition(newPosition);
+        }
+    }
 }
